eee81e986084df7a48d593bd76b9e372f5002b61
Moshe Maor <Moshe.maor@kaltura.com>
2020-02-25 19:04:42 +0200

    Config

--- ConfigReader.js
+++ ConfigReader.js
@@ -1,6 +1,6 @@
 function ConfigReader(configFile)
 {
-  const config = require('configFile');
+  const config = require(configFile);
   const obj = JSON.parse(config);
   console.log("from config json" + obj);
   return obj;
