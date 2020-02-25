2ca20aa78cd9d8ccc5db31fbf4f11db3e4900a5c
Inbal Ben David <inbal.bendavid@kaltura.com>
2020-02-25 17:46:53 +0200

    wait for functions

--- record.js
+++ record.js
@@ -1,15 +1,3 @@
-function startRec(ks)
-{
-  console.log('startRec function started');
-  var url = 'https://www.kaltura.com/api_v3/service/uploadtoken/action/add?ks=' + ks
-  fetch(url, {
-    method: 'post',
-  }).then(function(data) {
-    console.log('startRec at response', data.json);
-    return data.json()});
-}
-
-
 function uploadChunk(ks, token, fileData, finalChunk)
 {
   console.log('uploadChunk function started');
@@ -21,24 +9,49 @@ function uploadChunk(ks, token, fileData, finalChunk)
     return response.json()});
 }
 
-function addEntry(ks)
+function attachEntryAndToken(ks)
 {
-  console.log('addEntry function started');
-  var url = 'https://www.kaltura.com/api_v3/service/baseentry/action/add?format=1&type=1&entry%3AobjectType=KalturaBaseEntry&entry%3A&ks=' + ks
+  console.log('addEntry started');
+  var url = 'https://www.kaltura.com/api_v3/service/baseentry/action/add?format=1&type=1&entry%3AobjectType=KalturaBaseEntry&entry%3A&ks=' + ks;
   fetch(url, {
     method: 'post',
-  }).then(function(data) {
-    console.log('addEntry at response', data.json);
-    return data.json()});
+  }).then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    var entryId = json['id'];
+    createToken(ks, entryId);
+
+  });
 }
 
 function addContent(ks, entryId, token)
 {
-  console.log('addContent function started');
+  console.log('addContent started');
   var url = 'https://www.kaltura.com/api_v3/service/baseentry/action/addContent?resource%3AobjectType=KalturaUploadedFileTokenResource&resource%3Atoken=' + token + '&entryId=' + entryId + '&ks=' + ks
   fetch(url, {
     method: 'post',
-  }).then(function(data) {
-    console.log('addcContent at response', data.json);
-    return data.json()});
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    console.log(json);
+    console.log('addContent finished');
+  });
+}
+
+function createToken(ks, entryId)
+{
+  console.log('createToken started');
+  var url = 'https://www.kaltura.com/api_v3/service/uploadtoken/action/add?format=1&ks=' + ks;
+  fetch(url, {
+    method: 'post',
+  }).then(function(response) {
+    return response.json();
+  })
+      .then(function(json) {
+        console.log(json);
+        var token = json['id'];
+        addContent(ks, entryId, token)
+      });
 }
