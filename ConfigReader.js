c5c93f731d0e52f6778b683e33cfad2ebd82a707
ravitshalem <ravit.shalem@kaltura.com>
2020-02-25 12:22:53 +0200

    Update ConfigReader.js

--- ConfigReader.js
+++ ConfigReader.js
@@ -1,9 +1,6 @@
 class ConfigReader
 {
-  var Server = require('mongodb').Server;
-  var MongoDB = require('mongodb').Db;
   var CONFIG = require('./config.json');
-
   var obj = JSON.parse(CONFIG);
 
   console.log("from config json" + obj);
