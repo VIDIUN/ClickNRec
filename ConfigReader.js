1bbff24786b1ae3af80bd524a9ae279f5ff42c2d
Moshe Maor <moshemery@gmail.com>
2020-02-25 18:23:14 +0200

    Update ConfigReader.js

--- ConfigReader.js
+++ ConfigReader.js
@@ -1,7 +1,7 @@
-class ConfigReader
+function ConfigReader(configFile)
 {
-  var CONFIG = require('./config.json');
-  var obj = JSON.parse(CONFIG);
-
+  const config = require('configFile');
+  const obj = JSON.parse(config);
   console.log("from config json" + obj);
+  return obj;
 }
