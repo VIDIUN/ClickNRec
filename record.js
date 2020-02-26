6c00c98b9901b0dbbbeba5f4aec2e0f82aa29dc4
Inbal Ben David <inbal.bendavid@kaltura.com>
2020-02-26 14:35:01 +0200

    Update record.js

--- record.js
+++ record.js
@@ -33,12 +33,12 @@ function getLocation() {
 //callback		
 function showPosition(position) {
 	var latlon = position.coords.latitude + "," + position.coords.longitude;
-	jQuery('#pos_lat').val(position.coords.latitude);
-	jQuery('#pos_lon').val(position.coords.longitude);
+	//jQuery('#pos_lat').val(position.coords.latitude);
+	//jQuery('#pos_lon').val(position.coords.longitude);
 	//var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyDXea4aF63P3bIAruuIMi_V--f6X9_Wv2A";
-	var img_url = "https://maps.googleapis.com/maps/api/staticmap?markers="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyDXea4aF63P3bIAruuIMi_V--f6X9_Wv2A";
+	//var img_url = "https://maps.googleapis.com/maps/api/staticmap?markers="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyDXea4aF63P3bIAruuIMi_V--f6X9_Wv2A";
 	console.log("Position: Latitude: " + position.coords.latitude +	" , Longitude: " + position.coords.longitude);
-	document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+	//document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
   
 	displayLocation(position.coords.latitude, position.coords.longitude);
 }
